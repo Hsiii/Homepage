e69964a62b7a01aa9f33de7e2e386ab4fb875326
Hsi <its.hsichen@gmail.com>
2026-05-09 22:53:44 +0800

    feat(links): add figma shortcut to dev category

--- src/constants/linkTree.tsx
+++ src/constants/linkTree.tsx
@@ -45,6 +45,7 @@ export const linkTree: CategoryData[] = [
         links: [
             'GitHub',
             'Crx',
+            'Figma',
             'Vercel',
             'Supabase',
             'Lucide',
